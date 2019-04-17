f36b8a9ef0683f471c1c94753c0c134770f2e657
Hubert <hubert.haznar@gmail.com>
2019-04-17 10:08:50 +0200

    Finalized one line Main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        //Menu.menu();
-
-        Board board = new Board();
-
-        board.printBoard();
+        Menu.menu();
     }
 }
