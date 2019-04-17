080e59f7657ea877cc6c5eb9a58edc0e56496026
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-04-17 13:12:24 +0200

    Add few improvements to shot module

--- src/HandleGame.java
+++ src/HandleGame.java
@@ -2,34 +2,46 @@ import java.util.Scanner;
 
 public class HandleGame
 {
+    private Player player = new Player();
+    private boolean turn = player.getTurn();
+    private int score = player.getScore();
+    private boolean gameIsRunning;
+
 
 
 
 
     public void runApp(){
+        Scanner userInput = new Scanner(System.in);
+        this.gameIsRunning = true;
+        player.setTurn(true);
         Board board = new Board();
-        Scanner userShotX = new Scanner(System.in);
-        Scanner userShotY = new Scanner(System.in);
-        board.printBoard();
-        int x = userShotX.nextInt();
-        int y = userShotY.nextInt();
-        shot(x, y);
-        board.printBoard();
-
-
+        while(this.gameIsRunning) {
+            board.printBoard();
+            System.out.print("Enter x position: ");
+            int x = userInput.nextInt();
+            System.out.print("Enter y position: ");
+            int y = userInput.nextInt();
+            System.out.println("Your score: " + player.getScore());
+            shot(x, y);
+        }
     }
 
     private void shot(int x, int y){
         square[][] board = Board.getGameBoard();
         if(board[x][y].getName() == "S") {
             board[x][y].setName("X");
+            board[x][y].setDestroyed(true);
+            player.setScore(this.score += 1);
+            System.out.println("Hit!");
+
+        } else {
+            System.out.println("Miss!");
+            player.setTurn(false);
         }
-        System.out.println(board[x][y]);
-
-
-
-
-
     }
-    
+
+    public void setGameIsRunning(boolean gameIsRunning) {
+        this.gameIsRunning = gameIsRunning;
+    }
 }
