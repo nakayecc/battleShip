f0040a04c1334085285ca5a12f2bd653caff83db
Hubert <hubert.haznar@gmail.com>
2019-04-17 10:03:09 +0200

    Created HandleGame class

---
+++ src/HandleGame.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HandleGame
+{
+    private int x;
+    private int y;
+    private int hits;
+    private boolean isDestroyed;
+
+    Scanner userShot = new Scanner(System.in);
+    
+}
