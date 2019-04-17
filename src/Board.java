8e5a181f9eda81163846d3c4e64de2f98289ce41
Hubert <hubert.haznar@gmail.com>
2019-04-17 09:27:54 +0200

    Added print gameBoard with boardSquares inside grid

--- src/Board.java
+++ src/Board.java
@@ -11,19 +11,44 @@ public class Board {
             }
         }
 
-        gameBoard[1][1].setName("S");
         gameBoard[1][2].setName("S");
-        gameBoard[1][3].setName("S");
+        gameBoard[2][2].setName("S");
+        gameBoard[3][2].setName("S");
 
     }
 
 
     public void printBoard() {
-        for (int i = 0; i < boardHeight; i++) {
-            for (int j = 0; j < boardWidth; j++) {
+        char pipeVertical = '\u2502';
+        char pipeHorizontal = '\u2015';
+        String topMarks = "ABCDEFGHIJ";
+        String sideMarks = "0123456789";
+
+        String separator = "";
+
+        int n = 42;
+        for(int i=0; i < n; i++)
+        {
+            separator = separator + pipeHorizontal;
+        }
+
+        for (int i = 0; i < topMarks.length(); i++)
+        {
+            System.out.print("   " + topMarks.charAt(i));
+        }
+        System.out.println();
+
+        for(int i = 0; i < gameBoard.length; i++)
+        {
+            System.out.println(separator);
+            System.out.print(sideMarks.charAt(i));
+            for(int j = 0; j < gameBoard.length; j++)
+            {
+                System.out.print(pipeVertical + " ");
                 System.out.print(gameBoard[i][j].getName() + " ");
             }
-            System.out.println("\n");
+            System.out.println(pipeVertical);
         }
+        System.out.println(separator);
     }
 }
