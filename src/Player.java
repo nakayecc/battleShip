1dcdb7b33fa5e2645bcf6a27551f6598ff28d160
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-04-17 11:31:13 +0200

    Add player class with his lives and score

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    private int lives = 10;
+    private int score = 0;
+
+    public int getLives() {
+        return lives;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
