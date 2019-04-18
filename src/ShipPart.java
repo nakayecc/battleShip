6c3e6b82a829cb68b4cb6301707cb7e8cb2564d6
Michał Nabielec <michal.nabie@gmail.com>
2019-04-18 15:02:35 +0200

    Create ship and shipPart

---
+++ src/ShipPart.java
@@ -0,0 +1,27 @@
+public class ShipPart {
+    private int x;
+    private int y;
+    private boolean isHit;
+
+    public ShipPart(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.isHit = false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setHit(boolean hit) {
+        isHit = hit;
+    }
+
+    public boolean isHit() {
+        return isHit;
+    }
+}
